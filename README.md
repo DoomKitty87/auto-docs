8e88de8bf2bedb06b47ffc64c0afedd0d35b1607
DoomKitty87 <doomkitty887@gmail.com>
2023-05-26 09:12:48 -0700

    file input and explanation in readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # auto-docs
+Automatic documentation for any code, following simple formatting guidelines. To get documentation for your code, just run main.cpp in the directory it is in and provide the file name.
+
+Formatting:
+All lines to be read must start with comments.
+
+Functions should be formatted as:
+//Ffunction 2 processes two parameters into nothing
+(F for function, function is the name, and 2 for number of parameters, then description.)
+void function(type1 param1, type2 param2) {
+  return;
+}
+
+Add footnotes as:
+//Ncontent of footnote
--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <string>
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
+
+void document_file(string file_name) {
+  ifstream file_read;
+  file_read.open(file_name);
+  vector<string> commentlines = vector<string>();
+  string line;
+  while (getline(file_read, line)) {
+    if (line[0] == '/' && line[1] == '/') {
+      commentlines.push_back(line);
+    }
+  }
+
+  for (int i = 0; i < commentlines.size() / commentlines[0].size(); i++) {
+    
+  }
+}
 
 int main() {
   system("Color 03");
@@ -14,5 +32,6 @@ int main() {
     string file_name;
     cin >> file_name;
     cout << "Generating documentation for " + file_name + "." << endl;
+    document_file(file_name);
   }
 }
